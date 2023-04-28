fa2688ba4a9bb64d5c20a25f78590b0fc4221d74
Aleksandr Poliakov <master@artwist.ru>
2023-04-28 20:26:13 +0300

    lauchscreen is ready

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        sleep(4)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
