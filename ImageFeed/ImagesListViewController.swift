7618beb6c3ee31034dee5edd4afc8640de07b6ab
Aleksandr Poliakov <master@artwist.ru>
2023-04-30 21:45:32 +0300

    Имплементировали интерфейсы UITableViewDelegate и UITableViewDataSource в ImagesListViewController

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -12,11 +12,33 @@ class ImagesListViewController: UIViewController {
     @IBOutlet private var imagesTable: UITableView!
     
     override func viewDidLoad() {
-        sleep(4)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
+}
+
+// MARK: UITableViewDataSource
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+
+// MARK: UITableViewDelegate
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 
 }
+
 
