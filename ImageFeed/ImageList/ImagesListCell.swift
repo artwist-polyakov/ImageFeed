55bb458291c549cd50d18704bce53536d23f2cbe
Aleksandr Poliakov <master@artwist.ru>
2023-05-01 12:32:41 +0300

    Пришлось очень долго мучаться, чтобы сверстать всё и заставить работать, даже авторское решение не помогло сразу

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Александр Поляков on 30.04.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
