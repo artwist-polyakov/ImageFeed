120b21a6c30332309664af14b86a2911aa3b8e04
Aleksandr Poliakov <master@artwist.ru>
2023-05-11 17:24:40 +0300

    Создан еще один вью контроллер для экрана профиля пользователя. Добавлены ассеты.

---
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Поляков on 11.05.2023.
+//
+
+
+import UIKit
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
