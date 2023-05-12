1cc1922057d95cd20fd495be2b339af3ccc8a7cc
Aleksandr Poliakov <master@artwist.ru>
2023-05-12 19:37:25 +0300

    Сверстали SingleImageViewController

---
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Поляков on 12.05.2023.
+//
+
+import UIKit
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var bigSinglePicture: UIImageView!
+}
