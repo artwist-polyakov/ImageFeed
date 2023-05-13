7e8ef5b7e66a014cc17f5505790b8ce4f71e9763
Aleksandr Poliakov <master@artwist.ru>
2023-05-13 11:47:23 +0300

    Добавлена кнопка назад в PresentViewController

--- ImageFeed/ImageList/SingleImageViewController.swift
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -7,11 +7,19 @@
 
 import UIKit
 final class SingleImageViewController: UIViewController {
-    var image: UIImage!
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            bigSinglePicture.image = image
+        }
+    }
     @IBOutlet var bigSinglePicture: UIImageView!
     
     override func viewDidLoad() {
             super.viewDidLoad()
             bigSinglePicture.image = image
         }
+    @IBAction private func didTabBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
