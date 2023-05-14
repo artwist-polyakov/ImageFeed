36f3eac65def49b78b5ffb3cee14d958b5ca4c97
Aleksandr Poliakov <master@artwist.ru>
2023-05-14 23:27:40 +0300

    Отцентрировали изображение

--- ImageFeed/ImageList/SingleImageViewController.swift
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -11,6 +11,7 @@ final class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             bigSinglePicture.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     @IBOutlet var bigSinglePicture: UIImageView!
@@ -30,10 +31,26 @@ final class SingleImageViewController: UIViewController {
             scrollView.minimumZoomScale = 0.1
             scrollView.maximumZoomScale = 1.25
             bigSinglePicture.image = image
-            scrollView.contentSize = screenSize
-            bigSinglePicture.contentMode = .scaleAspectFill
-            bigSinglePicture.frame = scrollView.bounds
+        rescaleAndCenterImageInScrollView(image: image)
         }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+
     
 }
 
