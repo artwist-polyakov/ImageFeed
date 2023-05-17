e10ee46f780b5987c87adbe7c7cf59af279c3a3d
Aleksandr Poliakov <master@artwist.ru>
2023-05-17 19:58:59 +0300

    Свёрстан кодом экран профиля

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -8,16 +8,91 @@
 
 import UIKit
 class ProfileViewController: UIViewController {
-    
-    @IBOutlet var ProfileUserNameLabel: UILabel!
-    @IBOutlet var logOutButton: UIButton!
-    @IBOutlet var ProfileNickNameLabel: UILabel!
-    @IBOutlet var ProfileStatusLabel: UILabel!
-    
+    private var label: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "YP Black")
+        let profileImage = UIImage(named: "ProfilePhoto") ?? UIImage(named: "ProfilePhotoPlaceholder")
+        let profilePhotoView = UIImageView(image: profileImage)
+        if profileImage ==  UIImage(named: "ProfilePhotoPlaceholder") {
+            profilePhotoView.tintColor = UIColor(named: "YP Gray")
+        }
+        
+        view.addSubview(profilePhotoView)
+        
+        profilePhotoView.translatesAutoresizingMaskIntoConstraints = false
+        profilePhotoView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        profilePhotoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
+        profilePhotoView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        profilePhotoView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        let logOutButton = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(Self.didTapLogoutButton)
+        )
+        
+        logOutButton.tintColor = UIColor(named: "YP Red")
+        view.addSubview(logOutButton)
+        logOutButton.translatesAutoresizingMaskIntoConstraints = false
+        logOutButton.centerYAnchor.constraint(equalTo: profilePhotoView
+            .centerYAnchor).isActive = true
+        logOutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24).isActive = true
+        
+        
+        let userName = UILabel()
+        userName.text = "Екатерина Новикова"
+        userName.textColor = UIColor(named: "YP White")
+        let boldFontSize: CGFloat = 23
+        let boldFont = UIFont.systemFont(ofSize: boldFontSize, weight: .bold)
+        userName.font = boldFont
+        view.addSubview(userName)
+        userName.translatesAutoresizingMaskIntoConstraints = false
+        userName.topAnchor.constraint(equalTo: profilePhotoView.bottomAnchor, constant: 8).isActive = true
+        userName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        
+        let userNickName = UILabel()
+        userNickName.text = "@ekaterina_nov"
+        userNickName.textColor = UIColor(named: "YP Gray")
+        let regularFontSize: CGFloat = 13
+        let regularFont = UIFont.systemFont(ofSize: regularFontSize, weight: .regular)
+        userNickName.font = regularFont
+        view.addSubview(userNickName)
+        userNickName.translatesAutoresizingMaskIntoConstraints = false
+        userNickName.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 8).isActive = true
+        userNickName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        
+        let userDescription = UILabel()
+        userDescription.text = "Hello, world!"
+        userDescription.textColor = UIColor(named: "YP White")
+        userDescription.font = regularFont
+        view.addSubview(userDescription)
+        view.addSubview(userDescription)
+        userDescription.translatesAutoresizingMaskIntoConstraints = false
+        userDescription.topAnchor.constraint(equalTo: userNickName.bottomAnchor, constant: 8).isActive = true
+        userDescription.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        
+        
+        
     }
-    @IBAction func logOutButtonTap(_ sender: Any) {
+    
+    @objc
+    private func didTapLogoutButton() {
+        for view in view.subviews {
+            if view is UILabel {
+                view.removeFromSuperview()
+            } else {
+                if let imageView = view as? UIImageView {
+                    imageView.image = UIImage(named: "ProfilePhotoPlaceholder")
+                    imageView.tintColor = UIColor(named: "YP Gray")
+                }
+            }
+            
+            
+        }
     }
 }
+
+
+
