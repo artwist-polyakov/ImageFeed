547c2aac1a39e21300c605eee0430181b4b4d445
Aleksandr Poliakov <master@artwist.ru>
2023-05-28 18:02:23 +0300

    Создали класс для авторизации

---
+++ ImageFeed/Authorization/AuthViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Поляков on 28.05.2023.
+//
+
+import Foundation
+class AuthViewController: UIViewController {
+    
+    
+    
+    
+    
+}
