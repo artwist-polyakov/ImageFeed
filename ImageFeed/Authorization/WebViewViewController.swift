840b54fe9dcd0c253327d6813f5abd7b0595870a
Aleksandr Poliakov <master@artwist.ru>
2023-05-31 19:06:19 +0300

    Загружается авторизационный экран

--- ImageFeed/Authorization/WebViewViewController.swift
+++ ImageFeed/Authorization/WebViewViewController.swift
@@ -15,5 +15,20 @@ class WebViewViewController: UIViewController {
     @IBAction private func didTapBackButton(_ sender: Any) {
     }
     
+    override func viewDidLoad() {
+        // документация тут https://unsplash.com/documentation/user-authentication-workflow
+        // нас интересует фрагмент https://share.cleanshot.com/yMSRhPDtVgxzhnZ7xWfp
+        let UnsplashAuthorizeURLString: String = "https://unsplash.com/oauth/authorize"
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+        URLQueryItem(name: "client_id", value: AccessKey),
+        URLQueryItem(name: "redirect_uri", value: RedirectURI),
+        URLQueryItem(name: "response_type", value: "code"),
+        URLQueryItem(name: "scope", value: AccessScope)
+        ]
+        let url = urlComponents.url!
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
     
 }
