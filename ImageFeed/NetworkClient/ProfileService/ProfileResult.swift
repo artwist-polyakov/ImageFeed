d8e0c9945e0705a18a2edd16ba92fc4e22e6aa13
Aleksandr Poliakov <master@artwist.ru>
2023-06-11 22:19:38 +0300

    Переместил блок авторизации в папку NetworkClient

---
+++ ImageFeed/NetworkClient/ProfileService/ProfileResult.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileResult.swift
+//  ImageFeed
+//
+//  Created by Александр Поляков on 11.06.2023.
+//
+
+struct ProfileResult:Codable {
+    let username: String
+    let firstName: String
+    let lastName: String
+    
+}
---
+++ ImageFeed/NetworkClient/ProfileService/ProfileService.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileService.swift
+//  ImageFeed
+//
+//  Created by Александр Поляков on 11.06.2023.
+//
+
+final class ProfileService {}
