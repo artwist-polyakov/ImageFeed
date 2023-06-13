765df82e265532f4b86a47de0684e1ccc958d5bb
Aleksandr Poliakov <master@artwist.ru>
2023-06-13 23:43:23 +0300

    Удалил ненужные принты

--- ImageFeed/NetworkClient/ProfileService/ProfileService.swift
+++ ImageFeed/NetworkClient/ProfileService/ProfileService.swift
@@ -19,9 +19,7 @@ final class ProfileService {
         _ token: String,
         completion: @escaping (Result<Profile, Error>) -> Void
     ) {
-        print("До ассерта")
         assert(Thread.isMainThread)
-        print("После ассерта")
         if task != nil {
             print("Таск == нил")
             task?.cancel()
@@ -32,12 +30,10 @@ final class ProfileService {
         }
         let task = object(for: selfProfileRequest) { [weak self] result in
             DispatchQueue.main.async {
-                print("Я тута")
-                print(result)
+
                 guard let self = self else { print("тут гард"); return }
                 switch result {
                 case .success(let body):
-                    print("Тут успех")
                     let profile = Profile(username: body.username, name: "\(body.firstName ?? "") \(body.lastName ?? "")", loginName: "@\(body.username)", bio: body.bio ?? "")
                     self.profile = profile
                     var imageProfileRequest: URLRequest {
@@ -50,9 +46,7 @@ final class ProfileService {
                     
                     completion(.success(profile))
                     self.task = nil
-                    print("Вот какой профиль \(String(describing: self.profile))")
                 case .failure(let error):
-                    print("Тут не успех")
                     completion(.failure(error))
                     self.task = nil
                 }
