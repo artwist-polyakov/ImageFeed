44d43f4ffd4ed5dd73c1d19653eabbe5b67af13e
Aleksandr Poliakov <master@artwist.ru>
2023-06-27 20:02:10 +0300

    Создали таргет для тестов

---
+++ ImageFeedTests/ImagesListServiceTests.swift
@@ -0,0 +1,15 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Александр Поляков on 27.06.2023.
+//
+@testable import ImageFeed
+import XCTest
+
+final class ImagesListServiceTests: XCTestCase {
+
+    func testExample() {
+        }
+
+}
