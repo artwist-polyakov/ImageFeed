2e8ae58e6b58db31e5841731242fa3432bd03177
Aleksandr Poliakov <master@artwist.ru>
2023-06-27 20:42:53 +0300

    Тест на заполнение массива фотографий пройден

--- ImageFeed/NetworkClient/ImagesService/ImagesListService.swift
+++ ImageFeed/NetworkClient/ImagesService/ImagesListService.swift
@@ -31,33 +31,37 @@ final class ImagesListService {
         var photosPageRequest: URLRequest {
             URLRequest.makeHTTPRequest(path: "/photos", httpMethod: "GET", needToken: true, parameters: parameters)
         }
-        
+        print("ImagesListService: запрашиваю изображения с параметрами: \(parameters)")
         let task = urlSession.objectTask(for: photosPageRequest) { [weak self] (result: Result<PhotoResult, Error>) in
-            
+            print("ImagesListService запущена задача")
             DispatchQueue.main.async {
                 
-                guard let self = self else { print("тут гард"); return }
+                guard let self = self else { print("ImagesListService тут гард"); return }
                 switch result {
                 case .success(let body):
+                    print("ImagesListService: обновляю фотографии, текущая длина массива фото \(self.photos.count)")
                     self.photos += body.photoList.map {
                         Photo(id: $0.id,
-                              size: CGSize(width: $0.width,height: $0.height),
+                              size: CGSize(width: Double($0.width) ,height: Double($0.height)),
                               createdAt: $0.createdAt,
                               welcomeDescription: $0.description,
                               thumbImageURL: $0.urls.thumb ?? "",
                               largeImageURL: $0.urls.full ?? "",
                               isLiked: $0.isLiked)
                     }
+                    print("ImagesListService: обновил фотографии, текущая длина массива фото \(self.photos.count)")
                     NotificationCenter.default.post(
                         name: ImagesListService.DidChangeNotification,
                         object: self,
                         userInfo: ["photos": self.photos])
                     self.task = nil
                 case .failure(let error):
-                    print(error)
+                    print("ImagesListService ОШИБКА \(error)")
                     self.task = nil
                 }
             }
         }
+        self.task = task
+        task.resume()
     }
 }
--- ImageFeedTests/ImagesListServiceTests.swift
+++ ImageFeedTests/ImagesListServiceTests.swift
@@ -9,7 +9,18 @@ import XCTest
 
 final class ImagesListServiceTests: XCTestCase {
 
-    func testExample() {
+    func testFetchPhotos() {
+        let service = ImagesListService()
+        let expectation = self.expectation(description: "Wait for notification")
+        
+        NotificationCenter.default.addObserver(
+            forName: ImagesListService.DidChangeNotification,
+            object: nil,
+            queue: .main) { _ in
+                expectation.fulfill()
+            }
+        service.fetchPhotosNextPage()
+        wait(for: [expectation], timeout: 10)
+        XCTAssertEqual(service.photos.count, 10)
         }
-
 }
