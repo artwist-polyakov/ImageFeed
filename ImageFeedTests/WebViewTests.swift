efa9ae72d7399f76683fa17ca4e07f1cebb2fed8
Aleksandr Poliakov <master@artwist.ru>
2023-07-10 22:11:42 +0300

    Тест вебвью проходит

--- ImageFeedTests/WebViewTests.swift
+++ ImageFeedTests/WebViewTests.swift
@@ -9,11 +9,19 @@ import XCTest
 
 final class ImageFeedTests: XCTestCase {
     func testViewControllerCallsViewDidLoad() {
+        
+        // given
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
         let presenter = WebViewPresenterSpy()
         viewController.presenter = presenter
         presenter.view = viewController
+        
+        // when
+        _ = viewController.view
+        
+        // then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
     }
     
 }
